a594e614ef06b456c1b5794fc76678c4174f6563
vishalsingh17 <vishal170997@gmail.com>
2022-09-21 19:46:33 +0530

    Updated s3_operations.py

---
+++ air_pressure/s3_bucket_operations/s3_operations.py
@@ -0,0 +1,14 @@
+import json
+import os
+import pickle
+from io import StringIO
+
+import boto3
+import pandas as pd
+from botocore.exceptions import ClientError
+
+
+
+
+class S3_Operations:
+    pass
