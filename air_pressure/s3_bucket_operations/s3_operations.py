695d934037d186721c9d5bec1632d113b75d56d8
vishalsingh17 <vishal170997@gmail.com>
2022-09-21 19:52:56 +0530

    Updated s3_operations.py

--- air_pressure/s3_bucket_operations/s3_operations.py
+++ air_pressure/s3_bucket_operations/s3_operations.py
@@ -1,3 +1,4 @@
+import imp
 import json
 import os
 import pickle
@@ -7,8 +8,15 @@ import boto3
 import pandas as pd
 from botocore.exceptions import ClientError
 
-
-
+from utils.logger import App_Logger
+from utils.read_params import get_log_dic, read_params
 
 class S3_Operations:
-    pass
+    """
+    Class Name  :   S3_Operations
+    Description :   This method is used for all the S3 bucket operations
+    Written by  :   Vishal Singh
+    
+    Version     :   1.2
+    Revisions   :   Moved to setup to cloud
+    """
