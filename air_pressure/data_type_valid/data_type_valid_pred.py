02795f79c043005b7032c5c5f537acf7f78d40fa
Vishal Singh <55878408+vishalsingh17@users.noreply.github.com>
2022-09-27 12:54:28 +0530

    Updated data_type_valid_train.py

--- air_pressure/data_type_valid/data_type_valid_pred.py
+++
@@ -1,4 +0,0 @@
-from air_pressure.mongodb_operations.mongo_operations import MongoDB_Operation
-from air_pressure.s3_bucket_operations.s3_operations import S3_Operation
-from utils.logger import App_Logger
-from utils.read_params import get_log_dic, read_params
